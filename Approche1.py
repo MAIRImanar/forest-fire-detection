20de8ee443b58f1b20d7d672b44ab246df5b9feb
MAIRImanar <162744477+MAIRImanar@users.noreply.github.com>
2026-05-01 14:03:00 +0200

    Update Approche1.py

--- Approche1.py
+++ Approche1.py
@@ -40,7 +40,7 @@ print(f"Classes trouvees       : {os.listdir(CLASS_TRAIN)}")
 # 2. CONFIG
 
 BATCH_SIZE  = 32
-NUM_EPOCHS  = 15
+NUM_EPOCHS  = 1
 LR          = 0.001
 NUM_CLASSES = 2
 IMG_SIZE    = 224
@@ -240,7 +240,7 @@ print("\n[4/6] Entrainement YOLOv11s...")
 yolo = YOLO("yolo11s.pt")
 yolo.train(
     data     = DETECT_YAML,
-    epochs   = 50,
+    epochs   = 1,
     imgsz    = 640,
     batch    = 16,
     name     = "approche1_yolo",
