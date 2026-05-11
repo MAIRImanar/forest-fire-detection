d717a3f9e8f0449a123d113d4bc1edac2715e085
MAIRImanar <162744477+MAIRImanar@users.noreply.github.com>
2026-05-11 19:19:15 +0200

    Update Approche2.py

--- Approche2.py
+++ Approche2.py
@@ -74,7 +74,7 @@ print("\n[1/6] Fine-tuning YOLO-Classify sur dataset Shamta & Demir...")
 yolo_cls = YOLO("yolo11s-cls.pt")
 yolo_cls.train(
     data          = CLASS_TRAIN,
-    epochs        = 50,
+    epochs        = 1,
     imgsz         = 224,
     batch         = 32,
     name          = "approche2_classify",
