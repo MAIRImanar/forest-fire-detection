a58f657b9f733e6488d747f59106ffe2488a0b8c
MAIRImanar <162744477+MAIRImanar@users.noreply.github.com>
2026-05-14 14:56:21 +0200

    Update yolo2.py

--- yolo2.py
+++ yolo2.py
@@ -32,7 +32,7 @@ def find_real_path(relative_path):
 
 BASE_CLS = find_real_path("MEMOIRE/ForestFireDataset(Classifications)/ForestFireDataset")
 BASE_DET = find_real_path("MEMOIRE/ForesFireDataset(ObjectDetection)")
-BASE_OUT = find_real_path("MEMOIRE/YOLO2_YOLO11_Only_1epochs")
+BASE_OUT = find_real_path("MEMOIRE/YOLO2_YOLO11_Only_100epochs")
 
 # ---------------------------------------------
 # CRÉER SPLIT 70/15/15 AUTOMATIQUEMENT
@@ -144,7 +144,7 @@ yolo_cls = YOLO(CLS_BASE_MODEL)
 yolo_cls.train(
     task     = "classify",
     data     = SPLIT_DIR,    # ✅ dossier pas yaml
-    epochs   = 1,
+    epochs   = 100,
     imgsz    = 224,
     batch    = 32,
     lr0      = 0.001,
@@ -279,7 +279,7 @@ yolo_det = YOLO(DET_PRETRAINED)
 yolo_det.train(
     task          = "detect",
     data          = DETECT_YAML,
-    epochs        = 1,
+    epochs        = 100,
     imgsz         = 640,
     batch         = 16,
     lr0           = 0.001,
